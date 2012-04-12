6926e47db831bbce5565ead0136cc86e9992e888
Jaison George <jaisong87@gmail.com>
2012-04-12 10:08:38 -0500

    more parallel code for seqAlign

--- Prefix_SUM/prefixSum.cpp
+++ Prefix_SUM/prefixSum.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #define CHECK_FOR_CORRECTNESS 1
-#define MIN(a,b) ((a<b)?(a):(b))
+#define MIN(a,b) (( (a) < (b) )?(a):(b))
 
 /*  Kernel Function1 - Initialize the array */
 __global__ void initializeArray(int* A, int* B, int N)
@@ -14,21 +14,25 @@ printf("Setting B[%d] = %d , from A[%d] = %d\n", i, B[i], i, A[i]);
 
 /* Kernel Function2 - PrefixOperations on B */
 __global__ void prefixOnB(int* B, int t, int s)
-	{
+{
 	int i = threadIdx.x;
 	B[t + i] = MIN(B[s + 2*i - 1] , B[s + 2*i]);
-	}
+}
 
 /* kernel Function3 - PrefixOperations on C */
-__global__ void prefixOnB(int* B, int* C int t, int s)
+__global__ void prefixOnC(int* B, int* C,int t, int s)
 {
 	int i = threadIdx.x;
 	if (1 == i) 
 		 C[t + i] = B[t + i];
-   	else if(i%2 == 0) 
+   	else if((i%2) == 0) 
+		{
 		C[t + i] = C[s + (i>>1)];
-	else C[t + i] = MIN(C[s +((i−1)>>2)] , B[t + i]);
-}
+		}
+	else {
+		C[t + i] = MIN(C[s +((i-1)>>1)] , B[t + i]);
+		}
+}	
 
 /*  Kernel Function4 - Copy the results */
 __global__ void copyArray(int* S, int* C, int N)
@@ -38,6 +42,16 @@ int i = threadIdx.x;
 printf("Setting S[%d] = %d , from C[%d] = %d\n", i, S[i], i, C[i]);
 }
 
+
+/* Compute prefix sum of A into B 
+ * @param N - size of array A
+ * @param A - Initial array over which prefixSum should be calculated
+ * @param B - Array into which prefix Sum has to be calculated
+ */
+void computePrefixSum(int * A, int* B, int N)
+{
+
+}
 
 /* Just a somple function to get log to base 2*/
 int log2(int x)
@@ -50,7 +64,7 @@ return k;
 /* Main function - All of the implementation is in main */
 int main()
 {
-int N = 1024;
+int N = 64;
 
 /* Declare and Initialize host arrays A, B, S */
 int* h_A, *h_S, *h_B;
@@ -67,9 +81,13 @@ cudaMalloc(&d_B, 2*arrSize);
 cudaMalloc(&d_C, 2*arrSize);
 cudaMalloc(&d_S, arrSize);
 
-for(int i =0;i<N;i++)
-	h_A[i] = N-i;
+int seed = 1078989; int mod = 32768, step = 7986721;
 
+for(int i =0;i<N;i++)
+	{
+	h_A[i] = seed%mod;
+	seed+=step;
+	}
 /* Copy vectors from host memory to device memory */
 cudaMemcpy(d_A, h_A, arrSize, cudaMemcpyHostToDevice);
 
@@ -79,10 +97,13 @@ int blocksPerGrid = 1;
 initializeArray<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, N);
 
 #ifdef CHECK_FOR_CORRECTNESS
+for(int i1=0;i1<N;i1++)
+	printf("DBUG_0 %d \n", h_A[i1]);
+
 /* Checking for correctness */
 cudaMemcpy(h_B, d_B, 2*arrSize, cudaMemcpyDeviceToHost);
 for(int i=0;i<N;i++)
-	printf("%f %f\n", h_A[i], h_B[i]);
+	printf("DBUG_1  %d %d\n", h_A[i], h_B[i]);
 #endif
 
 int m = N, t = 0, h=1;
@@ -112,7 +133,12 @@ for(h=k;h>=0;h--)
 /* Copy the results from C */
 threadsPerBlock = N;
 blocksPerGrid = 1;
-copyArray<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, N);
+copyArray<<<blocksPerGrid, threadsPerBlock>>>(d_S, d_C, N);
+
+
+cudaMemcpy(h_S, d_S, arrSize, cudaMemcpyDeviceToHost);
+for(int i=0;i<N;i++)
+	printf("DBUG_2 %d %d\n", h_A[i], h_S[i]);
 
 /*Done with calculations - Free Device memory */
 cudaFree(d_A);
--- sequenceAlignment/sequenceAlignment.cpp
+++ sequenceAlignment/sequenceAlignment.cpp
@@ -1,74 +1,147 @@
-#include<iostream>
-#include<iomanip>
-#include<cstdio>
-using namespace std;
+#include<stdio.h>
+#define CHECK_FOR_CORRECTNESS 1
+#define MIN(a,b) (( (a) < (b) )?(a):(b))
 
-#define MIN(a,b) ( ((a)< (b))?(a):(b) )
-#define MAX 8192
-#define GE 1
-#define GI 2
-
-string X, Y;
-int N;
-int D[MAX][MAX], I[MAX][MAX], G[MAX][MAX];
-
-int S(int i, int j )
+/* Following section contains Kernel functions used by prefix sum */
+/*  Kernel Function1 - Initialize the array */
+__global__ void initializeArray(int* A, int* B, int N)
 {
-if(X[i] != Y[j]) return 1;
-return 0;
+int i = threadIdx.x;
+
+if(i<N) 
+	B[i] = A[i];
 }
 
-void print(int arr[MAX][MAX], string name)
+/* Kernel Function2 - PrefixOperations on B */
+__global__ void prefixOnB(int* B, int t, int s)
 {
-cout<<" =========== "<<name<<" ============ "<<endl;
-for(int i=0;i<N;i++, cout<<endl)
-	for(int j=0;j<N;j++)
-		cout<<setw(5)<<arr[i][j];
+	int i = threadIdx.x;
+	B[t + i] = MIN(B[s + 2*i - 1] , B[s + 2*i]);
+}
+
+/* kernel Function3 - PrefixOperations on C */
+__global__ void prefixOnC(int* B, int* C,int t, int s)
+{
+	int i = threadIdx.x;
+	if (1 == i) 
+		 C[t + i] = B[t + i];
+   	else if((i%2) == 0) 
+		{
+		C[t + i] = C[s + (i>>1)];
+		}
+	else {
+		C[t + i] = MIN(C[s +((i-1)>>1)] , B[t + i]);
+		}
+}	
+
+/*  Kernel Function4 - Copy the results */
+__global__ void copyArray(int* S, int* C, int N)
+{
+int i = threadIdx.x;
+        S[i] = C[i];
+//printf("Setting S[%d] = %d , from C[%d] = %d\n", i, S[i], i, C[i]);
+}
+
+/* Just a somple function to get log to base 2*/
+int log2(int x)
+{
+int k = 0;
+while(x>>=1) k++;
+return k;
+}
+
+/* Compute prefix sum of A into B 
+ * @param N - size of array A
+ * @param d_A - Initial device(CUDA)-array over which prefixSum should be calculated
+ * @param d_S - device(CUDA)-array into which prefix Sum has to be calculated
+ */
+void computePrefixSum(int * d_A, int* d_S, int N)
+{
+int * d_B, *d_C;
+size_t arrSize = N*sizeof(int);
+cudaMalloc(&d_B, 2*arrSize);
+cudaMalloc(&d_C, 2*arrSize);
+
+
+/* First call to Kernel Function to Initialize B */
+int threadsPerBlock = N;
+int blocksPerGrid = 1;
+initializeArray<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, N);
+
+/* A few variables required in prefix-computations */
+int m = N, t = 0, h=1;
+int k = log2(N);
+int s = 0;
+
+for(h =1; h<=k; h++)
+	{
+	s = t; t += m; m >>=1;
+	/* Second call to CUDA Kernel Function - This time logN calls. Every call has m parallel instances */
+	blocksPerGrid = 1;
+	threadsPerBlock = m;
+	prefixOnB<<<blocksPerGrid, threadsPerBlock>>>(d_B, t , s);			
+	}
+
+for(h=k;h>=0;h--)
+	{
+	blocksPerGrid = 1;
+        threadsPerBlock = m;
+	
+	/* Third call to kernel function - Again logN times m of them */
+	prefixOnC<<<blocksPerGrid, threadsPerBlock>>>(d_B, d_C, t , s); 	
+
+	m<<=1; s= t; t-=m;
+	}
+
+/* Copy the results from C */
+threadsPerBlock = N;
+blocksPerGrid = 1;
+copyArray<<<blocksPerGrid, threadsPerBlock>>>(d_S, d_C, N);
+
+/* Freeing two temporary device arrays B, C */
+cudaFree(d_B);
+cudaFree(d_C);
+
 return;
 }
 
+/* Main function - All of the implementation is in main */
 int main()
 {
-cin>>N;
-cin>>X>>Y;
+int N;
+char * X, *Y; /* char arrays in */
+char * d_X, *d_Y;
 
-X=X.substr(0,N);
-Y=Y.substr(0,N);
+/* Set of rows for matrices D, I, G and  arrays U, V  */
+/* Have two versions R0, R1 for every array and they are used interchangably in every iteration */
+int* d_D0, *d_D1, *d_I0, *d_I1, *d_G0, *d_G1, *d_U0, *d_U1, *d_V0, *d_V1;
 
-G[0][0] =0;
-for(int i=0;i<N;i++)
-	{
-		if(i>0) {
-			G[i][0] =  GI + GE*i;
-			G[0][i] =  GI + GE*i;
-			}
-		I[i][0] = G[i][0] + GE;
-		D[0][i] = GE + G[0][i];
-	}	
+scanf("%d",&N);
+size_t strSize = (N+1)*sizeof(char);
+X = (char*) malloc(strSize);
+Y = (char*) malloc(strSize);
+scanf("%s", X);
+scanf("%s", Y);
 
-for(int i=1;i<N;i++)
-	for(int j=0;j<N;j++)
-		{
-			if(i>0 && j>0) D[i][j] = MIN(D[i-1][j] , G[i-1][j] + GI )+GE;
-			
-			if(i>0 && j>0) I[i][j] = MIN(I[i][j-1], G[i][j-1]+GI )+GE;
-			
-			if(i>0 && j>0) { 
-				int mini  = MIN(D[i][j], I[i][j]);
-				int tmp = MIN(mini, G[i-1][j-1]+S(i,j));
-				G[i][j] = tmp;
-//			printf("Computed G[%d][%d] = %d from D[%d][%d] = %d, I[%d][%d] = %d, G[%d][%d] = %d, S[%d][%d] = %d \n" , i,j, G[i][j], i,j, D[i][j], i,j, I[i][j], i-1, j-1, G[i-1][j-1], i, j, S(i,j)); 
-				}
-		}
+/* Declare and Initialize device arrays d_X, d_Y */
+cudaMalloc(&d_X, strSize );
+cudaMalloc(&d_Y, strSize );
 
-//print(D, "Matrix-D");
-//print(I, "Matrix-I");
-//print(G, "Matrix-G");
+/* Copy vectors from host memory to device memory */
+cudaMemcpy(d_X, X, strSize , cudaMemcpyHostToDevice);
+cudaMemcpy(d_Y, Y, strSize , cudaMemcpyHostToDevice);
 
-int best = MIN(I[N-1][N-1], G[N-1][N-1]);
-best = MIN(best, D[N-1][N-1]);
-cout<<"Best alignment cost is "<<best<<endl;
-cout<<X<<endl;
-cout<<Y<<endl;
-return 0;	
+/*Initialize set of rows d_G0, d_I0, d_D0 */
+
+/*Done with calculations - Free Device memory */
+cudaFree(d_X);
+cudaFree(d_Y);
+
+/* Free host memory */
+free(X);
+free(Y);
+
+return 0;
 }
+
+
